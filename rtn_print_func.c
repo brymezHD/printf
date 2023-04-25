d4db1e4be6fb15312d2c63c03c8d17df1faa658d
brymezHD <hussainhebraheem@gmail.com>
2023-04-25 15:05:41 -0700

    print function

--- rtn_print_func.c
+++ rtn_print_func.c
@@ -5,7 +5,7 @@
  * @index: index of argument identifier.
  * Return: amount of identifiers.
  */
-int ev_print_func(const char *s, int index)
+int rtn_print_func(const char *s, int index)
 {
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str}, {"i", print_int},
