0e08f538eb44cbbc74e2f4da7eae28eea80852ed
brymezHD <hussainhebraheem@gmail.com>
2023-04-25 16:20:59 -0700

    print buffer

---
+++ print_buff.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_buff - prints buffer
+ * @buf: buffer pointer
+ * @nbuf: number of bytes to print
+ * Return: number of bytes printed.
+ */
+int print_buff(char *buf, unsigned int nbuf)
+{
+	return (write(1, buf, nbuf));
+}
